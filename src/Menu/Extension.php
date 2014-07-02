8978d372861999b4ba7c7f58bf0e9b418a85b6e4
Vít Kutný <vitkutny@outlook.com>
2014-07-03 00:16:48 +0200

    use class constant

--- src/Menu/Extension.php
+++ src/Menu/Extension.php
@@ -4,21 +4,23 @@ namespace WebEdit\Menu;
 
 use WebEdit\Bootstrap;
 use WebEdit\Translation;
+use WebEdit\Menu;
+use WebEdit\Menu\Group;
 
 final class Extension extends Bootstrap\Extension implements Translation\Provider {
 
     public function loadConfiguration() {
         $builder = $this->getContainerBuilder();
         $builder->addDefinition($this->prefix('repository'))
-                ->setClass('WebEdit\Menu\Repository');
+                ->setClass(Menu\Repository::class);
         $builder->addDefinition($this->prefix('facade'))
-                ->setClass('WebEdit\Menu\Facade');
+                ->setClass(Menu\Facade::class);
         $builder->addDefinition($this->prefix('control'))
-                ->setImplement('WebEdit\Menu\Control\Factory');
+                ->setImplement(Menu\Control\Factory::class);
         $builder->addDefinition($this->prefix('group.repository'))
-                ->setClass('WebEdit\Menu\Group\Repository');
+                ->setClass(Group\Repository::class);
         $builder->addDefinition($this->prefix('group.control'))
-                ->setImplement('WebEdit\Menu\Group\Control\Factory');
+                ->setImplement(Group\Control\Factory::class);
     }
 
 }
