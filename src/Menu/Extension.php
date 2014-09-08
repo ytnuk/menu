bba5a9ffaf676cca5720cd41bc826da66657c227
Vít Kutný <vitkutny@outlook.com>
2014-09-08 21:45:53 +0200

    dev

--- src/Menu/Extension.php
+++ src/Menu/Extension.php
@@ -2,14 +2,14 @@
 
 namespace WebEdit\Menu;
 
-use WebEdit\Bootstrap;
+use WebEdit\Module;
 use WebEdit\Application;
 use WebEdit\Database;
 use WebEdit\Translation;
 use WebEdit\Menu;
 use WebEdit\Menu\Group;
 
-final class Extension extends Bootstrap\Extension implements Application\Provider, Database\Provider, Translation\Provider {
+final class Extension extends Module\Extension implements Application\Provider, Database\Provider, Translation\Provider {
 
     public function getApplicationResources() {
         return [
