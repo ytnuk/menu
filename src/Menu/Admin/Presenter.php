62c21b5e230998a25a325bfd5aa90eca8a0d5552
Vít Kutný <vitkutny@outlook.com>
2014-10-01 00:14:38 +0200

    dev

--- src/Menu/Admin/Presenter.php
+++ src/Menu/Admin/Presenter.php
@@ -3,6 +3,7 @@
 namespace WebEdit\Menu\Admin;
 
 use WebEdit\Admin;
+use WebEdit\Database;
 use WebEdit\Menu;
 
 final class Presenter extends Admin\Presenter
@@ -10,12 +11,14 @@ final class Presenter extends Admin\Presenter
 
     private $repository;
     private $control;
+    private $databaseGridControl;
     private $menu;
 
-    public function __construct(Menu\Repository $repository, Menu\Control\Factory $control)
+    public function __construct(Menu\Repository $repository, Menu\Control\Factory $control, Database\Grid\Control\Factory $databaseGridControl)
     {
         $this->repository = $repository;
         $this->control = $control;
+        $this->databaseGridControl = $databaseGridControl;
     }
 
     public function actionEdit($id)
@@ -30,6 +33,11 @@ final class Presenter extends Admin\Presenter
     public function renderEdit()
     {
         $this['menu'][] = 'menu.admin.edit';
+    }
+
+    protected function createComponentDatabaseGrid()
+    {
+        return $this->databaseGridControl->create($this->repository);
     }
 
 }
