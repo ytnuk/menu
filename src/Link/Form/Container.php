d7b9123180ea3d818c8bebabbbcdf5e4dcf77d1e
Vít Kutný <vitkutny@icloud.com>
2015-05-19 00:45:39 +0200

    dev

--- src/Link/Form/Container.php
+++ src/Link/Form/Container.php
@@ -13,17 +13,4 @@ use Ytnuk;
 final class Container extends Ytnuk\Orm\Form\Container
 {
 
-	/**
-	 * @param Ytnuk\Form $form
-	 */
-	protected function attached($form)
-	{
-		parent::attached($form);
-		foreach ($this['aliases']->getContainers() as $alias) {
-			unset($alias['link']);
-		}
-		foreach ($this['parameters']->getContainers() as $parameter) {
-			unset($parameter['link']);
-		}
-	}
 }
