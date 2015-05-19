5440899720f072bb444b0b5ffa4e6f0f7b5bc72f
Vít Kutný <vitkutny@icloud.com>
2015-05-19 10:15:25 +0200

    dev

--- src/Menu/Repository.php
+++ src/Menu/Repository.php
@@ -15,4 +15,12 @@ use Ytnuk;
 final class Repository extends Ytnuk\Orm\Repository
 {
 
+	public function remove($entity, $recursive = FALSE)
+	{
+		if ($link = $entity->getValue('link')) {
+			$link->getRepository()->removeAndFlush($link, TRUE);
+		}
+
+		return parent::remove($entity, $recursive);
+	}
 }
