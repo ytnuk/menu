a04024b2366db18ec436afbf55db41c82f61826f
Vít Kutný <vitkutny@icloud.com>
2015-05-19 22:01:24 +0200

    dev

---
+++ src/Menu/Node/Primary/Form/Container.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ytnuk\Menu\Node\Primary\Form;
+
+use Ytnuk;
+
+/**
+ * Class Container
+ *
+ * @package Ytnuk\Menu
+ */
+final class Container extends Ytnuk\Orm\Form\Container
+{
+
+}
